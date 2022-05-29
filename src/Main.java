8ea7c4c9f0c66e02e6004a28473a2e806fdf70f3
tomaslinkina <altoma@mail.ru>
2022-05-29 18:49:37 +0500

    Добавление суммы пополнения

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 1500;
-        int topUp = 2100;
+        int amount = 100;
+        int topUp = 1100;
         int bonus = 0;
         if (topUp > 1000) {
             bonus = topUp / 100;
         }
-        System.out.println("Ваш баланс " + (amount + bonus));
+        System.out.println("Ваш баланс " + (amount + bonus + topUp));
     }
 }
